f7e06accfed34ffb1326c2d20f48e7e647765b31
Yash Pokar <yashpokar@gmail.com>
2018-09-07 20:26:15 +0530

    changes

--- xpath_generator/spider_generator.py
+++ xpath_generator/spider_generator.py
@@ -31,12 +31,12 @@ class SpiderGenerator(object):
 			return result
 
 
-if __name__ == '__main__':
-	generator = SpiderGenerator()
-	generator.look_inside('https://www.amazon.com/dp/B071YV88D9')
-	spider = generator.find(
-		price='$16.99 - $23.80',
-		description='Material: 95% Rayon , 5% Spandex.Stretchy,soft and comfy.',
-		reviews='1,044 customer reviews',
-	)
-	print(spider.generate())
+# if __name__ == '__main__':
+# 	generator = SpiderGenerator()
+# 	generator.look_inside('https://www.amazon.com/dp/B071YV88D9')
+# 	spider = generator.find(
+# 		price='$16.99 - $23.80',
+# 		description='Material: 95% Rayon , 5% Spandex.Stretchy,soft and comfy.',
+# 		reviews='1,044 customer reviews',
+# 	)
+# 	print(spider.generate())
